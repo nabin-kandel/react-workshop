b916338bbe02706ec71454d4e820bc516e3ea691
Nabin Kandel <nabinkandel@student.pu.edu.np>
2023-05-05 13:35:23 +0545

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import "./App.css";
-import Tweet from "./components/Tweet";
+import Header from "./components/Header";
+import "./App.css"
 const App = () => {
   return (
     <>
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
+      <div className="tweet">
+        <img
+          className="prsofile-pic"
+          src="https://placekitten.com/50/50"
+          alt="Profile Picture"
+        />
+        <div className="tweet-content">
+           <Header/>
+          <p className="tweet-text">This is a sample tweet!</p>
+          <img
+            className="tweet-image"
+            src="https://placekitten.com/200/150"
+            alt="Tweet Image"
+          />
+          <div className="tweet-footer">
+            <button className="btn-reply">Reply</button>
+            <button className="btn-retweet">Retweet</button>
+            <button className="btn-like">Like</button>
+            <button className="btn-share">Share</button>
+          </div>
+        </div>
+      </div>
+      <Header/>
+      <Header/><Header/><Header/>
+      <h1> Hello world</h1>
     </>
   );
 };
